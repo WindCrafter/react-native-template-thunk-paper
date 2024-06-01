99abff05dec8218a84fa47b4c127991e338dd4f0
ZipEnter <tranbaphuc1999@gmail.com>
2024-06-01 10:46:20 +0700

    add BTextMulti

--- src/components/base/multiText.base.tsx
+++ src/components/base/multiText.base.tsx
@@ -1,15 +1,49 @@
-import React, { useCallback, useRef } from "react";
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import React, {useMemo} from "react";
+import {StyleProp, TextStyle} from "react-native";
+import {Text} from "react-native-paper";
 
-interface IBTextMulti {
-  text?: string;
+interface IBTextMultiProps extends Omit<React.ComponentProps<typeof Text>, "children" | "style"> {
+    children?: React.ReactNode,
+    text?: string
+    [key: string]: any
 }
 
-export default function BTextMulti() {
-  return (
-    <View>
-      <Text>Hello</Text>
-    </View>
-  );
+
+type TMultiStyles = {
+    [key in `style${1 | 2 | 3 | 4 | 5}`]?: StyleProp<TextStyle>
+}
+
+
+/**
+ * If you want to have different styles in the same text, use the ||| separator (e.g., Hello |||every|||body).
+ * Additionally, provide style1, style2, ...stylex (unlimited x) corresponding to the number of text segments when we split the string by the ||| character.
+ * @param text
+ * @param children
+ * @param props
+ * @constructor
+ */
+export default function BTextMulti({text, children, ...props}: IBTextMultiProps & TMultiStyles) {
+
+    const content = useMemo(() => {
+        return text || children || ""
+    }, [text, children])
+
+    const renderMultiText = () => {
+        let contentSegments = (content as string).split("|||")
+        let currentStyle: StyleProp<TextStyle> | undefined = undefined
+        return contentSegments.map((contentSegment, index) => {
+            currentStyle = props[`style${(Math.min(index, 21)) + 1}`] || {}
+            return <Text key={index.toString()} style={currentStyle}>{contentSegment}</Text>
+        })
+    }
+
+    return (
+        <Text {...props}>
+            {typeof content !== "string" ?
+                content
+                :
+                renderMultiText()
+            }
+        </Text>
+    );
 }
--- src/screens/login/login.screen.tsx
+++ src/screens/login/login.screen.tsx
@@ -1,113 +1,88 @@
-import React, { useCallback, useRef } from "react";
-import { StyleSheet, View } from "react-native";
-import { Text } from "react-native-paper";
-import { MHS } from "constants/system/ui/sizes.ui.constant";
-import { useSystemTheme } from "helpers/hooks/system.hook";
-import { useAppDispatch } from "configs/store.config";
-import { ITheme } from "constants/system/ui/theme.constant";
-import BottomSheet, { BottomSheetBackdrop, BottomSheetView } from "@gorhom/bottom-sheet";
+import React, {useCallback, useRef} from "react";
+import {StyleSheet, View} from "react-native";
+import {Text} from "react-native-paper";
+import {MHS} from "constants/system/ui/sizes.ui.constant";
+import {useSystemTheme} from "helpers/hooks/system.hook";
+import {useAppDispatch} from "configs/store.config";
+import {ITheme} from "constants/system/ui/theme.constant";
+import BottomSheet from "@gorhom/bottom-sheet";
 import TextInputBase from "components/base/textInput.base";
 import ButtonBase from "components/base/button.base";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import BTextMulti from "components/base/multiText.base";
 
 
 export default function LoginScreen() {
-  const { styles } = useSystemTheme(createStyles);
-  const [text, setText] = React.useState("");
+    const {styles} = useSystemTheme(createStyles);
+    const [text, setText] = React.useState("");
 
-  const dispatch = useAppDispatch();
-  // ref
-  const bottomSheetRef = useRef<BottomSheet>(null);
-
-
-  function login() {
-    // GlobalHelper.showSnackBarHelper({
-    //   content: "Hello",
-    //   duration: 2000,
-    //   elevation: 5,
-    //   type: ESystemStatus.Warning
-    // });
-    bottomSheetRef.current?.expand();
-  }
+    const dispatch = useAppDispatch();
+    // ref
+    const bottomSheetRef = useRef<BottomSheet>(null);
 
 
-  // callbacks
-  const handleSheetChanges = useCallback((index: number) => {
-    console.log("handleSheetChanges", index);
-  }, []);
+    function login() {
+        // GlobalHelper.showSnackBarHelper({
+        //   content: "Hello",
+        //   duration: 2000,
+        //   elevation: 5,
+        //   type: ESystemStatus.Warning
+        // });
+        bottomSheetRef.current?.expand();
+    }
 
-  return (
-    <View style={styles.container}>
-      <Text variant={"displayMedium"} >{"Login"}</Text>
-      <TextInputBase
-        label="Email"
-        value={text}
-        style={styles.input}
-        onChangeText={text => setText(text)}
-        mode={"outlined"}
-      />
-      <TextInputBase
-        label="Password"
-        value={text}
-        style={styles.input}
-        onChangeText={text => setText(text)}
-        mode={"outlined"}
-        secureTextEntry
-      />
 
-      <ButtonBase onPress={login} mode={"contained"}>
-        Login
-      </ButtonBase>
+    // callbacks
+    const handleSheetChanges = useCallback((index: number) => {
+        console.log("handleSheetChanges", index);
+    }, []);
 
-      <BottomSheet
-        ref={bottomSheetRef}
-        enableDynamicSizing
-        onChange={handleSheetChanges}
-        enablePanDownToClose
-        animateOnMount
-        backdropComponent={(props) => (
-          <BottomSheetBackdrop
-            {...props}
-            disappearsOnIndex={-1}
-            appearsOnIndex={0}
-          />
-        )}
-        index={-1}
-      >
-        <BottomSheetView style={styles.contentContainer}>
-          <Text>Awesome 🎉</Text>
-          <Text>Awesome 🎉</Text>
-          <Text>Awesome 🎉</Text>
-          <Text>Awesome 🎉</Text>
-          <Text>Awesome 🎉</Text>
-          <Text>Awesome 🎉</Text>
-        </BottomSheetView>
-      </BottomSheet>
-    </View>
 
-  );
+    return (
+        <View style={styles.container}>
+            <Text variant={"displayMedium"}>{"Login"}</Text>
+            <TextInputBase
+                label="Email"
+                value={text}
+                style={styles.input}
+                onChangeText={text => setText(text)}
+                mode={"outlined"}
+            />
+            <TextInputBase
+                label="Password"
+                value={text}
+                style={styles.input}
+                onChangeText={text => setText(text)}
+                mode={"outlined"}
+                secureTextEntry
+            />
+
+
+            <BTextMulti style1={{color: "red", fontWeight:'bold'}} style2={{color: 'green', fontSize: 20}}
+                        style3={{color: "blue", fontSize: 10}}>{"hello |||every||| body"}</BTextMulti>
+
+            <ButtonBase onPress={login} mode={"contained"}>
+                Login
+            </ButtonBase>
+
+        </View>
+    );
 };
 
 
 const createStyles = (theme: ITheme) => {
-  return StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      paddingHorizontal: MHS._16,
-      gap: MHS._16,
-      backgroundColor: theme.colors.background
-    },
-    contentContainer: {
-      flex: 1,
-      alignItems: "center",
-      minHeight: 100
-    },
-    input: {
-      width: "100%",
-      borderRadius: 100
-    }
-  });
+    return StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            paddingHorizontal: MHS._16,
+            gap: MHS._16,
+            backgroundColor: theme.colors.background
+        }, contentContainer: {
+            flex: 1, alignItems: "center", minHeight: 100
+        }, input: {
+            width: "100%", borderRadius: 100
+        }
+    });
 };
 
