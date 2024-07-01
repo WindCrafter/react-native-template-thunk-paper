4ab0b3d3814b4b1c9fa48fd9b8fc15fca1a7f0de
ZipEnter <tranbaphuc1999@gmail.com>
2024-07-01 09:28:47 +0700

    update form input and login screen

--- src/components/form/textInput.form.tsx
+++ src/components/form/textInput.form.tsx
@@ -1,25 +1,41 @@
 import React, {useCallback} from "react";
-import {TextInput} from "react-native-paper";
-import {Control, useController} from "react-hook-form";
-
+import {HelperText, TextInput} from "react-native-paper";
+import {Control, RegisterOptions, useController} from "react-hook-form";
+import {View} from "react-native";
+import {BStyle} from "constants/system/ui/styles.constant";
 interface IFTextInputProps extends React.ComponentProps<typeof TextInput> {
   control: Control<any>
   name: string
+  defaultValue?: string
+  hint?: string
+  rules?: Omit<RegisterOptions<any, string>, "disabled" | "valueAsNumber" | "valueAsDate" | "setValueAs">
 }
 
 /**
  * FTextInput is Form TextInput
  * @param control
+ * @param info
+ * @param rules
+ * @param defaultValue
  * @param name
  * @param onChangeText
  * @param props
  * @constructor
  */
-export default function FTextInput({control, name, onChangeText, ...props}: IFTextInputProps): React.JSX.Element {
-  const {field, fieldState} = useController({
+export default function FTextInput({
+                                     control,
+                                     hint,
+                                     rules,
+                                     defaultValue = "",
+                                     name,
+                                     onChangeText,
+                                     ...props
+                                   }: IFTextInputProps): React.JSX.Element {
+  const {field, fieldState, formState} = useController({
     control: control,
-    defaultValue: "",
-    name
+    defaultValue: defaultValue,
+    name,
+    rules
   })
 
   const onChangeTextOverride = useCallback((text: string) => {
@@ -27,9 +43,28 @@ export default function FTextInput({control, name, onChangeText, ...props}: IFTe
     onChangeText?.(text)
   }, [field.onChange])
 
-  return <TextInput
-    onChangeText={onChangeTextOverride}
-    {...props}
-    value={field.value}
-  />;
+  return (
+      <View style={BStyle.fullWidth}>
+        <TextInput
+            onChangeText={onChangeTextOverride}
+            {...props}
+            error={fieldState.invalid}
+            value={field.value}
+            ref={field.ref}
+        />
+
+        {
+          hint ?
+              <HelperText type={fieldState.invalid ? "error" : "info"} visible>{hint}</HelperText>
+              :
+              (
+                  (rules !== undefined && fieldState.error?.message) ?
+                      <HelperText type={"error"}
+                                  visible={fieldState.invalid}>{fieldState.error?.message}</HelperText>
+                      :
+                      null
+              )
+        }
+      </View>
+  );
 }
