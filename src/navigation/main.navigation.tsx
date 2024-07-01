c7a216d3f11407ce6035bd95f4f401ec232f01d1
ZipEnter <tranbaphuc1999@gmail.com>
2024-07-01 09:10:52 +0700

    add app bar

--- src/navigation/main.navigation.tsx
+++ src/navigation/main.navigation.tsx
@@ -1,9 +1,10 @@
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import React from "react";
+import React, {useMemo} from "react";
 import {
     NAVIGATION_DETAIL_LOGS_BUG_SCREEN,
     NAVIGATION_HOME_SCREEN,
-    NAVIGATION_LOGS_BUG_SCREEN, NAVIGATION_RELEASE_LOGS_SCREEN
+    NAVIGATION_LOGS_BUG_SCREEN,
+    NAVIGATION_RELEASE_LOGS_SCREEN
 } from "constants/system/navigation.constant";
 import {useSystemTheme} from "helpers/hooks/system.hook";
 import HomeScreen from "screens/home/home.screen";
@@ -11,17 +12,25 @@ import LogsBugScreen from "screens/logsBugFileStorage/logsBug.screen";
 import DetailLogsBugScreen from "screens/logsBugFileStorage/detail.LogsBug.screen";
 import languages from "constants/system/languages";
 import ReleaseLogsScreen from "screens/releaseLogs/releaseLogs.screen";
+import DefaultAppbarComponent from "components/appbar/default.appbar.component";
+import {NativeStackHeaderProps} from "@react-navigation/native-stack/src/types";
 
 
 const StackNavigator = createNativeStackNavigator();
 
 const MainNavigator = () => {
-    const {styles} = useSystemTheme();
+    const {theme} = useSystemTheme();
 
+    const screenOptions = useMemo(() => {
+        return {
+            header: (props: NativeStackHeaderProps) => <DefaultAppbarComponent {...props} />,
+        }
+    }, [languages, theme])
 
     return (
         <StackNavigator.Navigator
             initialRouteName={NAVIGATION_HOME_SCREEN}
+            screenOptions={screenOptions}
         >
             <StackNavigator.Screen name={NAVIGATION_HOME_SCREEN} options={{title: languages.home.home}}
                                    component={HomeScreen}/>
